5daa972e1f57bba64ecef4cf025902a2484e5d6a
mtaut <146488331+mtaut@users.noreply.github.com>
2024-02-26 16:37:35 -0700

    Formatting JS
    Formatting code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 function newItem(){
-
+  /*
   //Vanilla JavaScript
   //1. Adding a new item to the list of items:
-    /*let li = document.createElement("li");
+    let li = document.createElement("li");
     let inputValue = document.getElementById("input").value;
     let text = document.createTextNode(inputValue);
     li.appendChild(text);
@@ -13,29 +13,29 @@ function newItem(){
     } else {
         let list = document.querySelector('#list');
         list.appendChild(li);
-    }*/
+    }
   
   //2. Crossing out an item from the list of items.
-    /*function crossOut() {
+    function crossOut() {
       li.classList.toggle("strike");
     }
 
-    li.addEventListener("dblclick",crossOut);*/
+    li.addEventListener("dblclick",crossOut);
 
   //3(i). Adding the delete button "X":
-    /*let crossOutButton = document.createElement("crossOutButton");
+    let crossOutButton = document.createElement("crossOutButton");
     crossOutButton.appendChild(document.createTextNode("X"));
     li.appendChild(crossOutButton);
 
-    crossOutButton.addEventListener("click", deleteListItem);*/
+    crossOutButton.addEventListener("click", deleteListItem);
 
   //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-    /*function deleteListItem(){
+    function deleteListItem(){
         li.classList.add("delete");
-    }*/
+    }
     
   //4. Reordering the items:
-    //$('#list').sortable();
+    $('#list').sortable();*/
 
   //--------------> jQuery Code below here:
 
@@ -73,4 +73,3 @@ function newItem(){
   //4. reordering the items:  
     $('#list').sortable();
 }
-
